5850177547b2d94795f13b6d1b7fcf6aae55a6af
Deeps <toskin.dv@gmail.com>
2025-06-16 19:23:53 +0300

    Добавлен проект с доменными моделями

---
+++ MiniBar/src/Minibar_Entities/Ratings/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minibar.Entities.Ratings
+{
+    internal class Rating
+    {
+        public Guid Id { get; set; }
+        public float Score { get; set; }
+        public List<Guid> UserIds { get; set; }
+        public Guid EntityId { get; set; }
+    }
+}
