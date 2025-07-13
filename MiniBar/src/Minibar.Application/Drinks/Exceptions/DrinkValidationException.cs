518cc37dd1a14533d0629424d39610197a99228b
Deeps <toskin.dv@gmail.com>
2025-07-13 20:41:04 +0300

    Промежуточные сохранения

---
+++ MiniBar/src/Minibar.Application/Drinks/Exceptions/DrinkValidationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Minibar.Application.Exceptions;
+using Shared;
+
+namespace Minibar.Application.Drinks.Exceptions
+{
+    public class DrinkValidationException : BadRequestException
+    {
+        public DrinkValidationException(Error[] errors)
+            : base(errors)
+        {
+        }
+    }
+}
---
+++ MiniBar/src/Minibar.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,14 @@
+﻿using Shared;
+using System.Text.Json;
+
+namespace Minibar.Application.Exceptions
+{
+    public class BadRequestException : Exception // 500
+    {
+        public BadRequestException(Error[] errors)
+            : base(JsonSerializer.Serialize(errors))
+        {
+
+        }
+    }
+}
