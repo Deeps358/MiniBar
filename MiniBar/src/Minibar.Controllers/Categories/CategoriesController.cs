d1490996685e4be77d8630f838125774126af7a0
Deeps <toskin.dv@gmail.com>
2025-08-02 18:30:41 +0300

    Дополнительная проверка роли и аутентификации

--- MiniBar/src/Minibar.Controllers/Categories/CategoriesController.cs
+++ MiniBar/src/Minibar.Controllers/Categories/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Minibar.Contracts.Categories;
 
 namespace Minibar.Controllers.Categories
@@ -14,18 +15,21 @@ namespace Minibar.Controllers.Categories
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> Create([FromBody] CreateCategoryDTO createCategoryDTO, CancellationToken cancellationToken)
         {
             return Ok("Category added");
         }
 
         [HttpPut("{categoryId:guid}")]
+        [Authorize(Roles = "Администратор")] // Только пользователи с ролью "Admin"
         public async Task<IActionResult> Update([FromRoute] Guid categoryId, [FromBody] UpdateCategoryDTO updateCategoryDTO, CancellationToken cancellationToken)
         {
             return Ok("Category updated");
         }
 
         [HttpDelete("{categoryId:guid}")]
+        [Authorize(Roles = "Администратор")] // Только пользователи с ролью "Admin"
         public async Task<IActionResult> Delete([FromRoute] Guid categoryId, CancellationToken cancellationToken)
         {
             return Ok("Category removed");
